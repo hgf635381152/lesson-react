a2c4c36bbd7f687e66cf0f133be99f79cee391a3
hgf635381152 <635381152@qq.com>
2020-07-18 21:49:19 +0800

    react-jd-day1

---
+++ react/react-jd/jd-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Sort from './component/sort/Sort'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sort />
+    </div>
+  );
+}
+
+export default App;
