4c2f956220d6391843b62c752625a1dd0006fefd
hgf635381152 <635381152@qq.com>
2020-07-22 14:44:21 +0800

    sanyuan-music

--- react/sanyuan-music/src/routes/index.js
+++ react/sanyuan-music/src/routes/index.js
@@ -7,26 +7,55 @@ import React from 'react'
 import { Redirect } from 'react-router-dom'
 import Recommend from '../application/Recommend'
 import BlankLayout from '../layouts/BlankLayout'
+import HomeLayout from '../layouts/HomeLayout'
 
-export default [
-  {
-    component: BlankLayout,
-    routes: [
-      {
-        path: '/',
-        exact: true,
-        render: () => <Redirect to={"/recommend"} />
-      },
-      {
-        path: '/recommend',
-        component: Recommend,
-        // routes: [
-        //   {
-        //     path: '/recommend/:id',
-        //     component: AlbumComponent
-        //   }
-        // ]
-      }
-    ]
-  }
-]
+export default [{
+  component: BlankLayout,
+  routes: [
+    {
+      path: "/",
+      component: HomeLayout,
+      routes: [
+        {
+          path: "/",
+          exact: true,
+          render: () => <Redirect to={"/recommend"} />
+        },
+        {
+          path: "/recommend",
+          component: Recommend
+        },
+        // {
+        //   path: "/singers",
+        //   component: SingersComponent
+        // },
+        // {
+        //   path: "/rank",
+        //   component: RankComponent
+        // }
+      ]
+    }
+  ]
+}]
+// [
+//   {
+//     component: BlankLayout,
+//     routes: [
+//       {
+//         path: '/',
+//         exact: true,
+//         render: () => <Redirect to={"/recommend"} />
+//       },
+//       {
+//         path: '/recommend',
+//         component: Recommend,
+//         // routes: [
+//         //   {
+//         //     path: '/recommend/:id',
+//         //     component: AlbumComponent
+//         //   }
+//         // ]
+//       }
+//     ]
+//   }
+// ]
