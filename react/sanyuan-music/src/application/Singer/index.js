195a71715e0164963ec74c967d575db8f5ec7956
hgf635381152 <635381152@qq.com>
2020-07-27 14:31:15 +0800

    sanyuanmusic update

--- react/sanyuan-music/src/application/Singer/index.js
+++ react/sanyuan-music/src/application/Singer/index.js
@@ -2,9 +2,11 @@ import React, { useEffect, useRef, memo } from 'react'
 import { connect } from 'react-redux'
 import { NavContainer } from './style'
 import { categoryTypes, alphaTypes } from '../../api/config'
+import Horizen from '../../baseUI/horizen-item/index';
 
 
 function Singers(props) {
+  const { category, alpha } = props;
   const handleUpdateCategory = () => {
 
   }
@@ -15,26 +17,21 @@ function Singers(props) {
   return (
     <div>
       <NavContainer>
-        <Horizen title="分类(默认热门):" list={categoryTypes} handleClick={(v) => handleUpdateCategory(v)}/>
-        <Horizen title="首字母:" list={alphaTypes} handleClick={(v) => handleUpdateAlpha(v)}/>
+        <Horizen title="分类(默认热门):" list={categoryTypes} handleClick={(v) => handleUpdateCategory(v)} oldVal={category}/>
+        <Horizen title="首字母:" list={alphaTypes} handleClick={(v) => handleUpdateAlpha(v)} oldVal={alpha}/>
       </NavContainer>
     </div>
   )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    prop: state.prop
-  }
-}
+const mapStateToProps = (state) => ({
+  category: state.singer.category,
+  alpha: state.singer.alpha
+})
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    dispatch1: () => {
-      dispatch()
-    }
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(memo(Singers))
 
--- react/sanyuan-music/src/store/reducer.js
+++ react/sanyuan-music/src/store/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { reducer as recommendReducer } from '../application/Recommend/store/index'
+import { reducer as singerReducer } from '../application/Singer/store/index'
 
 export default combineReducers({
-  recommend: recommendReducer
+  recommend: recommendReducer,
+  singer: singerReducer
 })
