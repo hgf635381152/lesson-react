dbbfabd578787fcd655f439ab8273b163c1fb793
hgf635381152 <635381152@qq.com>
2020-07-29 14:26:27 +0800

    sanyuanmusic

---
+++ react/sanyuan-music/src/application/Search/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+
+
+const Search = (props) => {
+  return (
+    <>
+    Search
+    </>
+  )
+}
+
+export default React.memo(Search)
